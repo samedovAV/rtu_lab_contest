ecf23f8cd2a4fa0354183f0f5f359a98e0aef077
samedovAV <alekcandr206@gmail.com>
2021-02-24 19:57:53 +0300

    Initial commit

---
+++ src/main/java/ru/samedov/rtulabcontest/RtuLabContestApplication.java
@@ -0,0 +1,13 @@
+package ru.samedov.rtulabcontest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtuLabContestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RtuLabContestApplication.class, args);
+    }
+
+}
